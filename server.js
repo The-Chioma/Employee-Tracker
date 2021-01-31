35a6e0672f8a29324c3ac91cfb39121a7d2fcdc3
Chioma Onyeguke <chiomaonyeguke1@gmail.com>
2021-01-31 17:54:47 +0000

    added functions to view all departments, employees, roles and total budget

--- server.js
+++ server.js
@@ -120,10 +120,34 @@ const addEmployee = () =>{
 
     // ])
 }
-const viewDepartments =()=>{}
-const viewRoles =()=>{}
-const viewEmployees =()=>{}
-const viewBudget =()=>{}
+const viewDepartments =()=>{
+    connection.query("SELECT * FROM department", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
+    });
+}
+const viewRoles =()=>{
+    connection.query("SELECT * FROM role", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
+    });
+}
+const viewEmployees =()=>{
+    connection.query("SELECT * FROM employee", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
+    });
+}
+const viewBudget =()=>{
+    connection.query("SELECT SUM (salary) FROM role", function(err, res) {
+        if (err) throw err;
+        console.table(res);
+        connection.end();
+    });
+}
 const updateRole =()=>{}
 
 // Start our server so that it can begin listening to client requests.
